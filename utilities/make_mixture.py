58fc8d0fd3283ac12229314f548959d30f7a9269
Jon Wedel <wedel@rendena.nmrfam.wisc.edu>
2018-08-31 13:48:36 -0500

    Finished mixture. Now supports relative concentrations.

--- utilities/make_mixture.py
+++ utilities/make_mixture.py
@@ -6,7 +6,6 @@ try:
     import simplejson as json
 except ImportError:
     import json
-from decimal import Decimal
 
 import optparse
 # Specify some basic information about our command
@@ -18,7 +17,13 @@ opt.add_option("--resolution", action="store", dest="resolution", type="int", de
                help="The number of points to calculate")
 opt.add_option("--file", action="store", dest="filename", default='mixture.csv',
                help="The file name to store the result.")
+opt.add_option("--precise", action="store_true", dest="precise", default=False,
+               help="Use exact precision. Way slower.")
 options, input_ids = opt.parse_args()
+
+# Use Decimal() rather than float
+if options.precise:
+    from decimal import Decimal as float
 
 
 class SpectralResolver:
@@ -29,13 +34,13 @@ class SpectralResolver:
     y = []
     x_position = 0
     
-    def __init__(self, file_location):
+    def __init__(self, file_location, scale=1):
         """ File location should be the full or relative path to the JSON file for the frequency you want."""
 
-        print('Loading file: %s' % file_location)
+        print('Loading file: %s of scale %s' % (file_location, scale))
         self.x, self.y = json.load(open(file_location, "r"))
         self.x = [float(_) for _ in self.x]
-        self.y = [float(_) for _ in self.y]
+        self.y = [scale*float(_) for _ in self.y]
 
         self.x_position = -1
 
@@ -64,13 +69,21 @@ class SpectralResolver:
         self.x_position = -1
 
 
-def get_mixture_spectra(frequency, resolution, gissmo_folder_list, filename=None):
+def get_mixture_spectra(frequency, resolution, gissmo_id_and_scale_tuple, filename=None):
     """ Returns [x_array, y_array] for the mixture with a number of points corresponding to the resolution if
-    no filename specified. Otherwise writes to specified filename in JSON format. """
+    no filename specified. Otherwise writes to specified filename in JSON format.
 
-    def path_get(gissmo_folder, frequency):
-        return os.path.join('/websites/gissmo/DB', gissmo_folder, 'simulation_1/spectral_data/', 'sim_%sMHz.json' % frequency)
-    resolvers = [SpectralResolver(path_get(x, frequency)) for x in gissmo_folder_list]
+     Input:
+       frequency, as string or int
+       resolution: number of points to generate in the range [-1,12]
+       gissmo_id_and_scale_tuple: A list of tuples of GISSMO ID and scale factor. Example:
+         [(bmse000001, 1), (bmse000002, .5)]
+       filename: The filename to write the results to as CSV. If omitted, returns the results to the caller
+     """
+
+    def get_path(gissmo_id):
+        return os.path.join('/websites/gissmo/DB', gissmo_id, 'simulation_1/spectral_data/', 'sim_%sMHz.json' % frequency)
+    resolvers = [SpectralResolver(get_path(x[0]), x[1]) for x in gissmo_id_and_scale_tuple]
     x_interval = 13 / float(resolution)
 
     y = [0]*options.resolution
@@ -89,5 +102,14 @@ def get_mixture_spectra(frequency, resolution, gissmo_folder_list, filename=None
 
 
 if __name__ == "__main__":
-    get_mixture_spectra(options.frequency, options.resolution, input_ids, options.filename)
+
+    def args_parse(gissmo_folder):
+        try:
+            id, scale = gissmo_folder.split(":")
+        except ValueError:
+            id, scale = gissmo_folder, 1
+        return id, float(scale)
+
+    gissmo_compound_list = [args_parse(x) for x in input_ids]
+    get_mixture_spectra(options.frequency, options.resolution, gissmo_compound_list, options.filename)
     print('Wrote results to file: %s' % options.filename)
