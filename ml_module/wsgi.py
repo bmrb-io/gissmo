77190c4004519df8ca7aeff0e438e721432e214b
Jon Wedell <jonwedell@gmail.com>
2019-09-10 14:18:36 -0500

    Fix numpy error.

--- ml_module/wsgi.py
+++ ml_module/wsgi.py
@@ -91,6 +91,7 @@ def simulate():
             write_spin_system(proton_indices, spin_matrix, input_parameters)
         except:
             input_parameters["err"] = "Something went wrong!"
+        input_parameters['spin_matrix'] = input_parameters['spin_matrix'].tolist()
         json.dump(input_parameters, open("params.json", "w"))
 
         os.system("zip outputs.zip params.json spectrum.csv spin_system.csv")
