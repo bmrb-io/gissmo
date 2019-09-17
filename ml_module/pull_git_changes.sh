ab237b9d8ed2cfdbfa4044e7e6267bbf451c62b8
Jon Wedell <jonwedell@gmail.com>
2019-09-17 08:55:21 -0500

    Exit 1 if cd fails.

--- ml_module/pull_git_changes.sh
+++ ml_module/pull_git_changes.sh
@@ -5,7 +5,7 @@ THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 GITDIFF=$(git rev-list HEAD...private/master --count)
 
 if [ "$GITDIFF" != "0" ]; then
-  cd "$THIS_DIR"
+  cd "$THIS_DIR" || exit 1
   git pull private master
   ./rebuild_docker.sh
 fi
