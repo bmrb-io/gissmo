97ef6db498974d456732ce40238bcd4f38e6583d
Jon Wedell <jonwedell@gmail.com>
2019-09-17 08:58:13 -0500

    Update pull_git_changes to fetch first.

--- ml_module/pull_git_changes.sh
+++ ml_module/pull_git_changes.sh
@@ -2,6 +2,7 @@
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+git fetch private
 GITDIFF=$(git rev-list HEAD...private/master --count)
 
 if [ "$GITDIFF" != "0" ]; then
